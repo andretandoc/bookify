64994f815f03c22c744c7fce44b135c1a58cd4e2
Teema-Ismail <teemaismail@gmail.com>
2024-12-09 11:24:27 -0500

    Style sheet updates & private CreateBooking page creation

---
+++ client/src/components/createBooking.jsx
@@ -0,0 +1,19 @@
+/* This is private page for creating a booking ; it should generate a URL -- Coming Soon*/
+
+/* for backend the form here will have the following elements */
+/*Class;Type;Professor; Date Range;Time Range */
+
+
+const CreateBooking = () => {
+  return (
+    <div className="private-page">
+      <main className="create-booking-main">
+        <div className="big-red-box">
+          <h2>Create Your Booking</h2>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default CreateBooking;
