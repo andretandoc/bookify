350f5f3676ee4cc7aa2fb032a157ce659848c129
unknown <andrevonvince@gmail.com>
2024-12-11 18:15:01 -0500

    added authentication + login redirects to MemberPage (backend)

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,11 +4,17 @@ const {
   registerMember,
   loginMember,
 } = require("../controllers/authController");
+const { authenticateToken } = require("../middleware/authMiddleware");
+//console.log(typeof authenticateToken);
 
 // Register route
 router.post("/register", registerMember);
 
 // Login route
 router.post("/login", loginMember);
+
+router.get("/private-data", authenticateToken, (req, res) => {
+  res.json({ message: "This is private data only for logged-in users." });
+});
 
 module.exports = router;
