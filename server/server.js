ec12d724b9f3c1eb260a8c2912be57329d32631a
Andre Tandoc <andrevonvince@gmail.com>
2024-12-16 17:06:36 -0500

    oops

--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ const app = express();
 
 const authRoutes = require("./routes/authRoutes");
 const appointmentRoutes = require("./routes/appointmentRoutes");
-const eventRoutes = require("./routes/eventRoutes"); // Path to the event routes
+// const eventRoutes = require("./routes/eventRoutes"); // Path to the event routes
+const customMeetingRoutes = require("./routes/customMeetingRoutes"); // Import the custom meeting routes
 
 // Middleware
 app.use(cors({ origin: "http://localhost:5173" })); // Allow requests from frontend running on port 5173
@@ -16,7 +17,8 @@ app.use(express.json()); // Parse JSON data in requests
 // Routes
 app.use("/api/auth", authRoutes); // Auth routes
 app.use("/api/appointments", appointmentRoutes); // Appointment routes
-app.use("/api/events", eventRoutes);
+// app.use("/api/events", eventRoutes);
+app.use("/api/custom-meetings", customMeetingRoutes); // Custom Meeting routes
 
 // Connect to MongoDB
 mongoose
