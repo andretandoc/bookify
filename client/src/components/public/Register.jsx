da2963cbeba788b7c766ef507cdd797b20cf5fd1
Andre Tandoc <andrevonvince@gmail.com>
2024-12-17 00:28:48 -0500

    fix /register endpoint on live

--- client/src/components/public/Register.jsx
+++ client/src/components/public/Register.jsx
@@ -27,16 +27,15 @@ function Register({ setIsLoggedIn }) {
     }
 
     try {
-      const response = await axios.post(
-        "http://localhost:5005/api/auth/register",
-        {
-          fname, 
-          lname,
-          email,
-          password,
-          confirmPassword: confirmation,
-        }
-      );
+      console.log("API URL:", import.meta.env.VITE_API_URL);
+      const API_URL = import.meta.env.VITE_API_URL;
+      const response = await axios.post(`${API_URL}/api/auth/register`, {
+        fname,
+        lname,
+        email,
+        password,
+        confirmPassword: confirmation,
+      });
 
       const token = response.data.token;
       localStorage.setItem("token", token);
@@ -49,85 +48,82 @@ function Register({ setIsLoggedIn }) {
   };
 
   return (
-    <main className = "layout">
-      <div className = "form-box">
-        <h1 class = "title">Register with Bookify</h1>
-        <form id = "register_form" className = "register-form" action = "" method="">
-          <div class = "input-text">
-            <div class = "fl-name">
+    <main className="layout">
+      <div className="form-box">
+        <h1 class="title">Register with Bookify</h1>
+        <form id="register_form" className="register-form" action="" method="">
+          <div class="input-text">
+            <div class="fl-name">
               <input
-                type = "text"
-                id = "fname"
-                name = "fname"
-                placeholder = "Enter first name"
-                value = {fname}
-                onChange = {(e) => setFirstName(e.target.value)}
+                type="text"
+                id="fname"
+                name="fname"
+                placeholder="Enter first name"
+                value={fname}
+                onChange={(e) => setFirstName(e.target.value)}
                 required
               />{" "}
               <input
-                type = "text"
-                id = "lname"
-                name = "lname"
-                placeholder = "Enter last name"
-                value = {lname}
-                onChange = {(e) => setLastName(e.target.value)}
+                type="text"
+                id="lname"
+                name="lname"
+                placeholder="Enter last name"
+                value={lname}
+                onChange={(e) => setLastName(e.target.value)}
                 required
               />{" "}
             </div>
-            <div className = "input">
+            <div className="input">
               <input
-                type = "text"
-                id = "email"
-                name = "email"
-                placeholder = "Enter email"
-                value = {email}
-                onChange = {(e) => setEmail(e.target.value)}
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
-                />{" "}
-            </div> 
-            <div className = "input">
-                <input
-                type = "password"
-                id = "password"
+              />{" "}
+            </div>
+            <div className="input">
+              <input
+                type="password"
+                id="password"
                 name="password"
-                placeholder = "Enter password"
-                value = {password}
-                onChange = {(e) => setPassword(e.target.value)}
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 required
-                />{" "}
+              />{" "}
             </div>
-            <div className = "input">
-                <input
-                type = "password"
-                id = "confirmation"
-                name = "confirmation"
-                placeholder = "Confirm password"
-                value = {confirmation}
-                onChange = {(e) => setConfirmation(e.target.value)}
+            <div className="input">
+              <input
+                type="password"
+                id="confirmation"
+                name="confirmation"
+                placeholder="Confirm password"
+                value={confirmation}
+                onChange={(e) => setConfirmation(e.target.value)}
                 required
-                />{" "}
+              />{" "}
             </div>
           </div>
-          {message && <p class = "error-message">{message}</p>}
-          <button class = "double-btn" type = "submit" onClick = {handleRegister}>
+          {message && <p class="error-message">{message}</p>}
+          <button class="double-btn" type="submit" onClick={handleRegister}>
             Sign Up
           </button>
-         
         </form>
         <div>
-            <p>
-              Already have an account?{" "}
-              <Link to = "/Login">&nbsp;&nbsp;Login</Link>
-            </p>
+          <p>
+            Already have an account? <Link to="/Login">&nbsp;&nbsp;Login</Link>
+          </p>
         </div>
       </div>
 
-      <div className = "footer">
-    <footer>
-        <p> &copy; 2024 Bookify! McGill University  </p>
-    </footer>
-    </div>
-
+      <div className="footer">
+        <footer>
+          <p> &copy; 2024 Bookify! McGill University </p>
+        </footer>
+      </div>
     </main>
   );
 }
