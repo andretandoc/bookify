afe7472f16191662a6840063a181a299b314dbd8
Leen Assaf <leenassa444@gmail.com>
2024-12-16 23:05:23 -0500

    fixed burger

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,97 @@
+// import image from "../icons/Bookify.jpeg";
+// import { Link } from "react-router-dom";
+// import { useState, useEffect, useRef } from "react";
+
+// function Header({ isLoggedIn, onLogout }) {
+//   const [showNavbar, setShowNavbar] = useState(false);
+//   const navbarRef = useRef(null);
+
+//   useEffect(() => {
+//     document.body.classList.toggle("nav-open", showNavbar);
+//     return () => {
+//       document.body.classList.remove("nav-open");
+//     };
+//   }, [showNavbar]);
+
+//   const toggleNavbar = () => {
+//     setShowNavbar(!showNavbar);
+//   };
+
+//   // Close the navbar when clicking outside of it
+//   useEffect(() => {
+//     const handleClickOutside = (event) => {
+//       if (
+//         navbarRef.current &&
+//         !navbarRef.current.contains(event.target) && 
+//         event.target.className !== "hamburger active" 
+//       ) {
+//         setShowNavbar(false);
+//       }
+//     };
+
+//     document.addEventListener("mousedown", handleClickOutside);
+//     return () => {
+//       document.removeEventListener("mousedown", handleClickOutside);
+//     };
+//   }, []);
+
+//   return (
+//     <div className = "header">
+//       <header>
+//         <div className = "logo">
+//           <Link to = "/" style={{ textDecoration: "none" }}>
+//             <img src = {image} alt = "Logo" />
+//           </Link>
+//           <span className = "tool-name">&nbsp;&nbsp;Bookify</span>
+//         </div>
+
+//         <button
+//           className = {`hamburger ${showNavbar ? "active" : ""}`}
+//           onClick = {toggleNavbar}>
+//           &#9776;
+//         </button>
+//         <nav
+//           className = {`navbar ${showNavbar ? "active" : ""}`}
+//           ref = {navbarRef} >
+//           {isLoggedIn ? (
+//             <>
+             
+//               <div className = "nav-item">
+//                 <button
+//                   className="double-btn"
+//                   onClick = {onLogout}
+//                  >
+//                   Log Out
+//                 </button>
+//               </div>
+//             </>
+//           ) : (
+//             <>
+//               <div className="nav-item">
+//                 <Link to="/">Home</Link>
+//               </div>
+//               <div className="nav-item">
+//                 <Link to="/ApptForm">History</Link>
+//               </div>
+//               <div className="nav-item">
+//                 <Link to="/PublicEvents">Events</Link>
+//               </div>
+//               <div className="nav-item">
+//                 <Link to="/Login">Login</Link>
+//               </div>
+
+//               <div className = "nav-item">
+//                 <Link to = "/Register">Register</Link>
+//               </div>
+//             </>
+//           )}
+//         </nav>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default Header;
 import image from "../icons/Bookify.jpeg";
 import { Link } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
@@ -55,12 +149,43 @@ function Header({ isLoggedIn, onLogout }) {
           ref = {navbarRef} >
           {isLoggedIn ? (
             <>
-             
+              <div className = "nav-item">
+                <Link to = "/MemberPage">Home</Link>
+              </div>
+
+              <div className = "nav-item">
+                <Link to = "/CustomMeeting">Custom Meeting</Link>
+              </div>
+
+              <div className = "nav-item">
+                <Link to = "/ManageBooking">Manage Meeting</Link>
+              </div>
+
+              <div className = "nav-item">
+                <Link to = "/ManageEvent">Manage Event</Link>
+              </div>
+
+              <div className = "nav-item">
+                <Link to = "/CreateEvent">Create Event</Link>
+              </div>
+
+              <div className = "nav-item">
+                <Link to = "/URLTest">BookingURL-Test</Link>
+              </div>
+
+              <div className = "nav-item">
+                <Link to = "/FullEvents">Book an Appointment</Link>
+              </div>
+
               <div className = "nav-item">
                 <button
-                  className="double-btn"
                   onClick = {onLogout}
-                 >
+                  style = {{
+                    background: "none",
+                    border: "none",
+                    cursor: "pointer",
+                    color: "inherit",
+                  }}>
                   Log Out
                 </button>
               </div>
@@ -92,4 +217,3 @@ function Header({ isLoggedIn, onLogout }) {
 }
 
 export default Header;
-
