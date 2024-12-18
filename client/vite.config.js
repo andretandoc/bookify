c54f98d7963fe11139fb242be4555399e28327ae
Andre Tandoc <andrevonvince@gmail.com>
2024-12-18 01:13:12 -0500

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // Ensures correct asset paths
   plugins: [react()],
-  root: ".", // Root folder
   build: {
-    outDir: "dist", // Output directory
-    rollupOptions: {
-      input: "./index.html", // Ensure index.html is the build entry point
-    },
+    outDir: "dist",
   },
 });
